1ec92eb14f1796e39e5ae595241b9e9484377ed2
AB SIDDIK <abft102ctg@gmai.com>
2026-08-25 00:19:53 +0600

    something changes

--- Trackable.java
+++ Trackable.java
@@ -1,6 +1,3 @@
-// Trackable.java
-// এই ইন্টারফেসটা Doctor আর Hospital — দুইটা সম্পূর্ণ unrelated ক্লাস implement করবে।
-// একটাই method: getStatus() — যে যার মতো নিজের status রিটার্ন করবে (polymorphism)।
 
 public interface Trackable {
     String getStatus();
